8c3d83cafaf6467ba1f7649d90bc39c280d4cc72
TanmoySarkar <tan99sarkar88@gmail.com>
2025-12-06 11:37:23 +0530

    new default browser - qutebrowser
    2. updated bright script and corresponding cron entry
    
    3. rearranged a few aliases in aliasrc
    
    4. legibility to bakp script
    
    5. other misc updates

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -83,7 +83,7 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       (spell +aspell +everywhere) ; tasing you for misspelling mispelling
+       (spell +flyspell +everywhere) ; tasing you for misspelling mispelling
        grammar           ; tasing grammar mistake every you make
 
        :tools
