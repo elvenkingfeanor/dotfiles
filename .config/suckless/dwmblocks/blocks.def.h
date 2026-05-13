6f9087a72530d84d14cf1aef4c93ef2fbdf7af4b
TanmoySarkar <tan99sarkar88@gmail.com>
2026-05-13 14:35:46 +0530

    updated weather scripts to rely on internal date data rather than modification time data
    rearranged crond/ts to have daily crons grouped together, then weekly crons, then monthly, finally annually
    
    rearranged modules in dwmblocks

--- .config/suckless/dwmblocks/blocks.def.h
+++ .config/suckless/dwmblocks/blocks.def.h
@@ -7,8 +7,8 @@ static const Block blocks[] = {
 	{"", "~/.local/bin/statusbar/freedisk",	60,		0},
 	{"", "~/.local/bin/statusbar/batt",	60,		0},
 	{"", "~/.local/bin/statusbar/cpumem",	10,		0},
-	{"", "~/.local/bin/statusbar/datime",	60,		0},
 	{"", "~/.local/bin/statusbar/wtr",	3600,		0},
+	{"", "~/.local/bin/statusbar/datime",	60,		0},
 	{"", "~/.local/bin/statusbar/bak",	0,		14},
 	{"", "~/.local/bin/statusbar/keebstate",	0,		13},
 };
